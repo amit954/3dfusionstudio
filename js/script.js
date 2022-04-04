e3cb29612a8ad79199a9e97c7b01c10e7cb62e7d
Amit Singh <singhamit954@gmail.com>
2022-04-04 17:48:22 +0530

    Update script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-grecaptcha.ready(function () {
-    if (grecaptcha.getResponse() === "") {
-        alert("Please validate the Google reCaptcha.");
-    } else {
-        alert("Successful validation! Now you can submit this form to your server side processing.");
-    }
-});
