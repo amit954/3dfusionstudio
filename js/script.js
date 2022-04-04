f760e958ef3928f2129a51b33d29f4ef9dc550a5
Amit Singh <singhamit954@gmail.com>
2022-04-04 18:05:32 +0530

    captcha form

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("captcha_form").addEventListener("submit",function(evt)
+  {
+
+  var response = grecaptcha.getResponse();
+  if(response.length == 0)
+  {
+    //reCaptcha not verified
+    alert("please verify you are human!");
+    evt.preventDefault();
+    return false;
+  }
+  //captcha verified
+  //do the rest of your validations here
+
+});
