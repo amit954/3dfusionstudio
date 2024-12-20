10cd07040ebe7fcde2fab2a37d4b88a50dd3a0e6
Amit Singh <singhamit954@gmail.com>
2024-12-20 14:24:51 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-document.getElementById("captcha_form").addEventListener("submit",function(evt)
-  {
-
-  var response = grecaptcha.getResponse();
-  if(response.length == 0)
-  {
-    //reCaptcha not verified
-
-    document.getElementById("recaptcha").style.display= "block" ;
-
-    evt.preventDefault();
-    return false;
-  }
-  //captcha verified
-  //do the rest of your validations here
-
-});
-
 document.addEventListener('DOMContentLoaded', function() {
   const navbar = document.querySelector('.navbar');
   const isHomePage = document.querySelector('.bgimage'); // Full height hero
